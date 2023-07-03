f7b31fe4acf84751f8fa09708de0ef72a5a92775
Pavel Parkhomenka <pawelparhomenko@gmail.com>
2023-07-03 19:28:59 +0300

    fix bug

--- src/index.js
+++ src/index.js
@@ -13,19 +13,21 @@ const url = 'https://cre-api.kufar.by/ads-search/v1/engine/v1/search/rendered-pa
 async function makeRequest() {
   try {
     const response = await axios.get(url)
-    const rooms = (await response.data).ads
+    const roomsUnOrder = (await response.data).ads
+    const rooms = roomsUnOrder.reverse()
 
-    fs.writeFileSync("lastRoom.json", JSON.stringify(rooms[0]))
+    fs.writeFileSync("lastRoom.json", JSON.stringify(roomsUnOrder[0]))
 
     let fileContent = fs.readFileSync("last.json", "utf8");
     const lastDate = JSON.parse(fileContent).lastDate
-    const trueRooms = []
+    let trueRooms = []
 
     for(let i = 0; i < rooms.length; i++) {
       if(new Date(lastDate).getTime() < new Date(rooms[i].list_time)) {
         trueRooms.push(rooms[i])
       }
     }
+
     if(trueRooms.length) {
       fs.writeFileSync("last.json", JSON.stringify({
         lastDate: trueRooms[0].list_time
@@ -41,16 +43,16 @@ const bot = new TelegramBot(process.env.TELEGRAM_API_TOKEN, {
   polling: true
 });
 
-bot.on("polling_error", err => console.log(err.data.error.message));
+bot.on("polling_error", err => console.error("polling_error"));
 
 bot.on('text', async msg => {
   await helpSendMessage(msg.chat.id, "Скоро всё будет");
 })
 
 bot.onText(/\/start/, async (msg) => {
-  const chatId = msg.chat.id; 10 * 1000
+  const chatId = msg.chat.id;
   await helpSendMessage(chatId, helpBuildString(...RES_START), "HTML")
-  setInterval(helpInterval, 5 * 60 * 1000, chatId)
+  setInterval(helpInterval, 30 * 60 * 1000, chatId)
 });
 
 bot.onText(/\/link/, async (msg) => {
@@ -90,8 +92,9 @@ bot.onText(/\/last/, async (msg) => {
   if(!lastRoom) {
     await helpSendMessage(chatId, "Последней квартиры ещё нет")
   }
-  await helpSendMessage(chatId, helpBuildString(...helpRoom(JSON.parse(lastRoom)))
-  )
+  else {
+    await helpSendMessage(chatId, helpBuildString(...helpRoom(JSON.parse(lastRoom))))
+  }
 })
 
 async function helpInterval(chatId) {
@@ -116,7 +119,7 @@ function helpRoom(room) {
     "Кол-во комнат: " + (room?.ad_parameters[9].vl || '-'),
     "Адрес: " + (room?.account_parameters.at(-1)?.v || '-'),
     "Тип валюты: " + (room?.currency || '-'),
-    "Дата: " + (new Date(room.list_time) || '-'),
+    "Дата: " + (getDateAndTime(new Date(room.list_time)) || '-'),
     "Стоимость: " + (room?.price_byn.slice(0, -2) || '-') + " BYN, " +
       (room?.price_usd.slice(0, -2) || '-') + "$",
     "\n" + (room?.subject || '-')
@@ -140,3 +143,13 @@ async function helpSendMessage(chatId, response, mode="") {
 }
 
 await bot.setMyCommands(COMMANDS)
+
+export function getDateAndTime(now) {
+  const day = now.getDate().toString().padStart(2, '0');
+  const month = (now.getMonth() + 1).toString().padStart(2, '0');
+  const year = now.getFullYear().toString().padStart(4, '0');
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  return `${day}.${month}.${year} ${hours}:${minutes}:${seconds}`
+}
